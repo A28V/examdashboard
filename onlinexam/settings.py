054b7ec4bb0c3ca202f75150d1f5893d881df320
A28V <28aug2022av@gmail.com>
2024-05-05 10:55:57 +0530

    delelte extra file

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -77,8 +77,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'onlinexam.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
